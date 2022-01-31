8fdc4f0eefe15387248b79fb26f25d53ad3b136a
K00237738 <K00237738@student.lit.ie>
2022-01-31 13:00:47 +0000

    start on projectiles

---
+++ SpaceShooter_DarkHorse/Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    protected Vector3 movement;
+    public float speed;
+    public bool isUpward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    protected void Movement()
+    {
+        movement = transform.position + Vector3.forward * Time.deltaTime * speed;
+        if(isUpward != true)
+        {
+            movement *= -1;
+        }
+        transform.position = movement;
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
